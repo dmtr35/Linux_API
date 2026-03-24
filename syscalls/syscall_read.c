7702d165fa6242a58710c3310b8e5fe804501de8
dmtr35 <nuke-id@yandex.ru>
2026-03-24 21:47:37 +0200

    first commit

---
+++ syscalls/syscall_read.c
@@ -0,0 +1,48 @@
+#define _GNU_SOURCE
+// #include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "/home/dm/WebstormProjects/c/Linux_API/lib/tlpi_hdr.h"
+#include "/home/dm/WebstormProjects/c/Linux_API/lib/error_functions.h"
+
+
+// Возвращает количество считанных байтов, 0 при EOF или –1 при ошибке
+// count определяет максимальное количество считываемых байтов
+// buffer предоставляет адрес буфера памяти, в который должны быть помещены входные данные
+ssize_t read(int fd, void *buffer, size_t count);
+
+
+
+
+#define MAX_READ 5
+
+int main()
+{
+    char buffer_1[MAX_READ];
+    int fd = open("startup", O_RDONLY);
+    if (fd == -1)
+        errExit("open");
+
+
+    ssize_t res = read(STDIN_FILENO, buffer_1, MAX_READ);
+    if (res == -1)
+        errExit("read");
+    printf("The input data was: %s\n", buffer_1);
+
+    char buffer_2[MAX_READ + 1];
+    ssize_t numRead = read(STDIN_FILENO, buffer_2, MAX_READ);
+    if (numRead == -1)
+        errExit("read");
+    buffer_2[numRead] = '\0';
+    printf("The input data was: %s\n", buffer_2);
+
+
+    return 0;
+}
+
+
+
+
+
+
+
---
+++ syscalls/syscall_write.c
@@ -0,0 +1,45 @@
+#define _GNU_SOURCE
+// #include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "/home/dm/WebstormProjects/c/Linux_API/lib/tlpi_hdr.h"
+#include "/home/dm/WebstormProjects/c/Linux_API/lib/error_functions.h"
+
+
+// Возвращает количество записанных байтов или –1 при ошибке
+ssize_t write(int fd, const void *buffer, size_t count);
+
+
+
+
+#define MAX_READ 5
+
+int main()
+{
+    char buffer_1[] = "hello world!\n";
+    int fd = open("startup", O_WRONLY);
+    if (fd == -1)
+        errExit("open");
+
+
+    ssize_t res1 = write(STDOUT_FILENO, buffer_1, MAX_READ);
+    if (res1 == -1)
+        errExit("write");
+    printf("The input data was: %s\n", buffer_1);
+
+    ssize_t res2 = write(fd, buffer_1, MAX_READ);
+    if (res2 == -1)
+        errExit("write");
+    printf("The input data was: %s\n", buffer_1);
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
