2a01be133f465ffd2280aef4367becaefb7abea8
dmtr35 <nuke-id@yandex.ru>
2026-04-16 19:41:43 +0300

    Auto-commit: 16-04-2026 19:41:43 Modified: memory/my_malloc.c

--- memory/my_malloc.c
+++ memory/my_malloc.c
@@ -7,20 +7,27 @@
 #include "/home/dm/WebstormProjects/c/Linux_API/lib/tlpi_hdr.h"
 #include "/home/dm/WebstormProjects/c/Linux_API/lib/error_functions.h"
 
-
+#define CHUNK_SIZE 20950
 // int brk(void *end_data_segment);
 // void *sbrk(intptr_t increment);
 
+typedef struct block {
+    size_t size;
+    int free;
+    struct block *next;
+} block_t;
+
 void *my_malloc(size_t size)
 {
-    size = (size + 15) & ~15;  // alignment
+    size = (size + 15) & ~15;
+    char *heap_start = sbrk(0);
+    char *heap_end = heap_start - CHUNK_SIZE;
 
-    void *cur = sbrk(0);
+    snprintf(heap_end - 8, 3, "%d", CHUNK_SIZE);
 
-    if (brk((char*)cur + size) == -1)
-        return NULL;
 
-    return cur;
+
+
 }
 
 int main(int argc, char *argv[])
@@ -31,19 +38,23 @@ int main(int argc, char *argv[])
     // printf("ptr: %p\n", ptr);
     // snprintf(ptr, 16, "%s", "hello_world!123");
     // printf("%s\n", ptr);
-
+    
     // printf("Initial program break: %10p\n", sbrk(0));
-
+    
     char *ptr_two = my_malloc(17);
     printf("ptr_two: %p\n", ptr_two);
     snprintf(ptr_two, 16, "%s", "hello_world!456");
     printf("%s\n", ptr_two);
+    base = sbrk(0);
+    printf("Initial program break: %10p\n", base);
     
     char *ptr_three = my_malloc(16);
     printf("ptr_three: %p\n", ptr_three);
     snprintf(ptr_three, 16, "%s", "hello_world!789");
     printf("%s\n", ptr_three);
-
+    base = sbrk(0);
+    printf("Initial program break: %10p\n", base);
+    
 
     return 0;
 }
