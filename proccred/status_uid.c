0cf31268ddda5912a71160bec89be75b0e9554a7
dmtr35 <nuke-id@yandex.ru>
2026-05-16 18:48:06 +0300

    Auto-commit: 16-05-2026 18:48:06 Added:    proccred/st Modified: proccred/01_info.c, proccred/status_uid, proccred/status_uid.c

--- proccred/status_uid.c
+++ proccred/status_uid.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE             /* getresuid, getresgid */
 #include <stdio.h>              /* printf, sprintf, snprintf */
 #include <unistd.h>             /* getuid, geteuid, setuid, read */
 #include <sys/types.h>          /* uid_t, gid_t */
@@ -7,6 +8,8 @@
 #include <fcntl.h>              /* open */
 #include <string.h>             /* strlen */
 
+// #include "/home/dm/WebstormProjects/c/Linux_API/lib/tlpi_hdr.h"
+#include "/home/dm/WebstormProjects/c/Linux_API/lib/error_functions.h"
 
 #define MAX_READ 64
 
@@ -47,21 +50,82 @@ int main()
   pid_t pid;
   
   pid = getpid();
-  printf_uid_status(pid);
   // ----------------------------------------------
   
-  uid_t real_id, effect_id;
+  uid_t uid, euid;
   int res;
   
-  real_id = getuid();
-  effect_id = geteuid();
-  printf("real_id: %d, efect_id: %d\n", real_id, effect_id);
+  uid = getuid();
+  euid = geteuid();
+  printf("real_id: %d, efect_id: %d\n", uid, euid);
   
-  res = setuid(real_id);
+  printf_uid_status(pid);             
+
+  // ===============================================================
+  /* setuid меняет id безвозвратно */
+  // res = setuid(1000);               // Uid:	1000	1000	1000	1000
+  // res = setgid(1000);               // Gid:	1000	1000	0	    1000
+  // printf_uid_status(pid);
+  // res = setuid(0);                  // Uid:	1000	1000	1000	1000
+  // res = setgid(0);                  // Gid:	1000	0	    0	    0
+  // printf_uid_status(pid);
+  
+  // ---------------------------------------------------------------
+  /*  способ для set-user-ID-root программы, чей действующий UID в этот момент равен 0,
+  безвозвратно сбросить все полномочия */
+  // if (setuid(getuid()) == -1)
+  //   errExit("setuid");
+  // printf_uid_status(pid);
+  // ===============================================================
+  
+  
+  
+  
+  // ===============================================================
+  /* изменения своего действующего пользовательского идентификатора 
+  (без изменения сохраненный установленного идентификатора */
+  // res = seteuid(1000);                  // Uid:	1000	1000	0	1000
+  // res = setegid(1000);                  // Gid:	1000	1000	0	1000
+  // printf_uid_status(pid);
+  // /* тоесть мы можем переключиться назад на привилегированный процесс */
+  // res = seteuid(0);                     // Uid:	1000	0     0 0
+  // res = setegid(0);                     // Gid:	1000	0     0 0
+  // printf_uid_status(pid);
+  // ===============================================================
+  
+  
+  // ===============================================================
+  /* Изменение реальных и действующих идентификаторов 
+     передать -1 чтобы оставить без изменения */
+  // res = setreuid(-1, 1000);               // Uid:	1000	1000	0	  1000
+  // printf_uid_status(pid);
+  // res = seteuid(0);                       // Uid:	1000	0	    0	  0
+  // printf_uid_status(pid);
+
+  // ---------------------------------------------------------------
+
+  /* лишиться своего привилегированного состояния безвозвратно */
+  // setreuid(getuid(), getuid());             // Uid:	1000	1000	1000	1000
+  // printf_uid_status(pid);
+  
+  // ===============================================================
+  
+  /* Извлечение реального, действительного и сохраненного установленного идентификаторов */
+  // int getresuid(uid_t *ruid, uid_t *euid, uid_t *suid);
+  uid_t ruid2, euid2, suid2;
+  res = getresuid(&ruid2, &euid2, &suid2);
+  printf("Uid:  %d  %d  %d\n", ruid2, euid2, suid2);       // Uid:  1000  0  0
+  puts("-------------------------------------------");
+
+  // ===============================================================
+
+  /*  Изменение реального, действительного и сохраненного установленного идентификаторов
+    Оба при успешном завершении возвращают 0 или -1 при ошибке */
+  res = setresuid(0, 1000, 0);                              // Uid:	0	1000	0	1000
   printf_uid_status(pid);
 
 
-
+  
   // pwd = getspnam("root");
   // printf("%s\n", pwd->sp_namp);
   // sleep(30003);
