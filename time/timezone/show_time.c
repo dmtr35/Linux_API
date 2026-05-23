78d6e1cb09b6a3b3791884cec13f7ea388332275
dmtr35 <nuke-id@yandex.ru>
2026-05-23 18:48:38 +0300

    Auto-commit: 23-05-2026 18:48:38 Added:    time/timezone/show_time, time/timezone/show_time.c, time/timezone/time_zone.c Modified: .vscode/tasks.json Deleted:  time/time_zones.c

---
+++ time/timezone/show_time.c
@@ -0,0 +1,33 @@
+#include <time.h>
+#include <locale.h>
+#include "/home/dm/WebstormProjects/c/Linux_API/lib/curr_time.h"
+#include "/home/dm/WebstormProjects/c/Linux_API/lib/tlpi_hdr.h"
+
+#define BUF_SIZE 200
+
+/* вызов программы с TZ
+    ./show_time
+    TZ=":Pacific/Auckland" ./show_time
+*/
+int main(int argc, char *argv[])
+{
+    time_t t;
+    struct tm *loc;
+    char buf[BUF_SIZE];
+
+    if (setlocale(LC_ALL, "C") == NULL)
+        errExit("setlocale");               /* Использование в преобразовании настроек локали */
+    t = time(NULL);
+        
+    printf("ctime() of time() value is: %s", ctime(&t));
+    loc = localtime(&t);
+    if (loc == NULL)
+        errExit("localtime");
+
+    printf("asctime() of local time is: %s", asctime(loc));
+    if (strftime(buf, BUF_SIZE, "%A, %d %B %Y, %H:%M:%S %Z", loc) == 0)
+        fatal("strftime returned 0");
+    printf("strftime() of local time is: %s\n", buf);
+
+    exit(EXIT_SUCCESS);
+}
