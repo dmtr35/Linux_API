59363c5a087765d1f7348944524014b06419dabc
dmtr35 <nuke-id@yandex.ru>
2026-06-13 21:23:41 +0300

    Auto-commit: 13-06-2026 21:23:41 Modified: 13_filebuff/my_tail.c, startup

--- 13_filebuff/my_tail.c
+++ 13_filebuff/my_tail.c
@@ -6,15 +6,18 @@
 #include <unistd.h>                             /* read, ssize_t, STDOUT_FILENO */
 #include "../lib/error_functions.h"
 
-#define MAX_READ 3
+#define MAX_READ 4
+#define MAX_ARR 4096
+// int arr_points[1000] = {0};
 
 int main(int argc, char *argv[])
 {
     int fd, buf_size = MAX_READ;
-    int n = 10, flag_f = 1;
+    int n = 10, flag_f = 1, i = 0;
     ssize_t res_read, res_write;
     char *file, *buf;
     off_t offset = 0, p;
+    int *arr_points;
 
     if (argc < 2) {
         usageErr("%s [ -f ] [ -n num ] file\n", argv[0]);
@@ -38,26 +41,43 @@ int main(int argc, char *argv[])
         }
     }
 
-    // ----------------------------------------------------
 
     fd = open(file, O_RDONLY);
     if (fd == -1)
         errExit("open");
 
+    arr_points = malloc(MAX_ARR);
+    if (arr_points == NULL)
+        errExit("malloc");
+
     buf = malloc(buf_size);
     if (buf == NULL)
         errExit("malloc");
 
+    // ----------------------------------------------------
+
     while ((res_read = read(fd, buf, MAX_READ)) > 0) {
-        if (res_read == -1)
-            errExit("read");
+        p = strcspn(buf, "\n");
+        if (buf[p] == '\n') {
+            arr_points[++i] = strcspn(buf, "\n");
+        }
+    }
+    
+    // ----------------------------------------------------
+
+
+
+    while ((res_read = read(fd, buf, MAX_READ)) > 0) {
         
         buf[res_read] = '\0';
 
         p = strcspn(buf, "\n");
+        offset += p;
+
         if (buf[p] == '\n') {
-            offset += p;
             lseek(fd, ++offset, SEEK_SET);
+        } else {
+            lseek(fd, offset, SEEK_SET);
         }
         
         res_write = write(STDOUT_FILENO, buf, ++p);
