03efb05e0236759f5d575b1d4abae24da06a423e
dmtr35 <nuke-id@yandex.ru>
2026-07-08 18:57:42 +0300

    Auto-commit: 08-07-2026 18:57:42 Added:    10_time/00_info.c, @ Modified: .vscode/launch.json, .vscode/tasks.json, 15_files/X_time.c Deleted:  10_time/info.c

--- 15_files/X_time.c
+++ 15_files/X_time.c
@@ -5,7 +5,7 @@
 #include <stdio.h>                      /* printf */
 #include <sys/stat.h>                   /* stat, lstat, fstat, utimensat */
 #include <sys/types.h>                  /* ino_t, dev_t, mode_t, uid_t, gid_t, off_t */
-#include <time.h>                       /* ctime */
+#include <time.h>                       /* ctime, strftime */
 #include <string.h>                     /* strstr, strcmp, strdup */
 #include <dirent.h>                     /* opendir, readdir */
 
@@ -55,7 +55,103 @@ void free_database(struct database *db)
 }
 
 
-void write_to_file(char *path, struct database *db)
+
+
+void print_time(const char *name, const struct timespec *ts)
+{
+    char buf[64];
+    struct tm tm;
+
+    localtime_r(&ts->tv_sec, &tm);
+
+    strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", &tm);
+
+    printf("%-15s %s.%09ld\n", name, buf, ts->tv_nsec);
+}
+
+void print_info(struct database *db)
+{
+    for (size_t i = 0; i < db->count; ++i) {
+        printf("%-15s %s\n", "Path", db->files[i].path);
+        printf("%-15s %ju\n", "I-node", (uintmax_t)db->files[i].inode);
+
+        print_time("Access:", &db->files[i].atime);
+        print_time("Modify:", &db->files[i].mtime);
+        print_time("Change:", &db->files[i].ctime);
+
+        puts("====================================================");
+    }
+}
+
+void add_to_arr(char *path, struct stat *sb, database *db)
+{
+    if(db->capacity == db->count) {
+        db->capacity *= 2;
+        
+        struct file_info *tmp = realloc(db->files, db->capacity * sizeof(struct file_info));
+
+        if (tmp == NULL)
+            errExit("realloc");
+
+        db->files = tmp;
+    }
+
+    db->files[db->count].path = strdup(path);
+    if (db->files[db->count].path == NULL)
+        errExit("strdup");
+
+    db->files[db->count].atime = sb->st_atim;
+    db->files[db->count].ctime = sb->st_ctim;
+    db->files[db->count].mtime = sb->st_mtim;
+    db->files[db->count].inode = sb->st_ino;
+    db->files[db->count].device = sb->st_dev;
+    db->files[db->count].uid = sb->st_uid;
+    db->files[db->count].gid = sb->st_gid;
+    db->files[db->count].mode = sb->st_mode;
+    db->files[db->count].size = sb->st_size;
+
+    (db->count)++;
+}
+
+void func_tree(char *path, database *db)
+{
+    struct stat sb;
+    DIR *info_dir;
+    struct dirent *dirent;
+
+    if(lstat(path, &sb) == -1)
+        errExit("lstat");
+    add_to_arr(path, &sb, db);
+
+    if ((info_dir = opendir(path)) == NULL)
+        errExit("opendir");
+    
+    
+    while(dirent = readdir(info_dir)) {
+        if(strcmp(dirent->d_name, ".") != 0 && strcmp(dirent->d_name, "..") != 0) {
+            size_t size_path = strlen(path) + strlen(dirent->d_name) + 2;
+            char *new_path = malloc(size_path);                                         // выделана память
+            if (new_path == NULL)
+                errExit("malloc");
+
+            snprintf(new_path, size_path, "%s/%s", path, dirent->d_name);
+
+            if(lstat(new_path, &sb) == -1)
+                errExit("lstat");
+
+            if (S_ISDIR(sb.st_mode)) {
+                func_tree(new_path, db);
+            } else {
+                add_to_arr(new_path, &sb, db);
+            }
+            free(new_path);
+        }
+    }
+    closedir(info_dir);
+}
+
+
+void write_to_file(char *path, struct database *db, int flag_info)
 {
     ssize_t res_wr;
     int fd;
@@ -97,95 +193,17 @@ void write_to_file(char *path, struct database *db)
         if (res == -1)
             errExit("write");
 
-        printf("%s\n", str_data);
+        // printf("%s\n", str_data);
         free(str_data);
     }
+
+    if(flag_info)
+        print_info(db);
 
     if(close(fd))
         errExit("close");
 }
 
-
-void print_info(struct database *db)
-{
-    for(int i = 0; i < db->count; ++i) {
-        printf("Path:                       %s\n", db->files[i].path);
-        printf("I-node number:              %ld\n", (long)db->files[i].inode);
-        printf("Last file access:           %s", ctime(&db->files[i].atime.tv_sec));
-        printf("Last file modification:     %s", ctime(&db->files[i].mtime.tv_sec));
-        printf("Last status change:         %s", ctime(&db->files[i].ctime.tv_sec));
-        printf("%s\n", "====================================================");          
-    }
-}
-
-
-void add_to_arr(char *path, struct stat *sb, database *db)
-{
-    if(db->capacity == db->count) {
-        db->capacity *= 2;
-        
-        struct file_info *tmp = realloc(db->files, db->capacity * sizeof(struct file_info));
-
-        if (tmp == NULL)
-            errExit("realloc");
-
-        db->files = tmp;
-    }
-
-    db->files[db->count].path = strdup(path);
-    if (db->files[db->count].path == NULL)
-        errExit("strdup");
-
-    db->files[db->count].atime = sb->st_atim;
-    db->files[db->count].ctime = sb->st_ctim;
-    db->files[db->count].mtime = sb->st_mtim;
-    db->files[db->count].inode = sb->st_ino;
-    db->files[db->count].device = sb->st_dev;
-    db->files[db->count].uid = sb->st_uid;
-    db->files[db->count].gid = sb->st_gid;
-    db->files[db->count].mode = sb->st_mode;
-    db->files[db->count].size = sb->st_size;
-
-    (db->count)++;
-}
-
-
-void func_tree(char *path, database *db)
-{
-    struct stat sb;
-    DIR *info_dir;
-    struct dirent *dirent;
-
-    if(lstat(path, &sb) == -1)
-        errExit("lstat");
-    add_to_arr(path, &sb, db);
-
-    if ((info_dir = opendir(path)) == NULL)
-        errExit("opendir");
-    
-    
-    while(dirent = readdir(info_dir)) {
-        if(strcmp(dirent->d_name, ".") != 0 && strcmp(dirent->d_name, "..") != 0) {
-            size_t size_path = strlen(path) + strlen(dirent->d_name) + 2;
-            char *new_path = malloc(size_path);                                         // выделана память
-            if (new_path == NULL)
-                errExit("malloc");
-
-            snprintf(new_path, size_path, "%s/%s", path, dirent->d_name);
-
-            if(lstat(new_path, &sb) == -1)
-                errExit("lstat");
-
-            if (S_ISDIR(sb.st_mode)) {
-                func_tree(new_path, db);
-            } else {
-                add_to_arr(new_path, &sb, db);
-            }
-            free(new_path);
-        }
-    }
-    closedir(info_dir);
-}
 
 void set_timespec(void *ts, char *str_data, int flag_type)
 {
@@ -203,7 +221,7 @@ void set_timespec(void *ts, char *str_data, int flag_type)
     }
 }
 
-void restor_meta(char *path_file, struct database *db)
+void restor_meta(char *path_file, struct database *db, int flag_info)
 {
     struct stat sb; 
     struct timespec times[2];
@@ -212,29 +230,29 @@ void restor_meta(char *path_file, struct database *db)
     dev_t saved_dev;
     ssize_t res;
     char buffer[MAX_PATH + 1] = {0};
-
+    
     fd = open(path_file, O_RDONLY);
     if(fd == -1)
-        errExit("open");
-
+    errExit("open");
+    
     while(res = read(fd, buffer, n)) {
         if((strchr(buffer, '\n')) == NULL) {
             n *= 2;
             if(lseek(fd, 0, SEEK_SET) == -1)
-                errExit("lseek");
+            errExit("lseek");
             if(n > LIMIT)
                 fatal("limit memory");
-        } else {
-            char *ptr;
-            size_t end = strcspn(buffer, "\n");
+            } else {
+                char *ptr;
+                size_t end = strcspn(buffer, "\n");
             char *str_data = malloc(end + 1);
             if(str_data == NULL)
-                errExit("malloc");
-
+            errExit("malloc");
+            
             strncpy(str_data, buffer, end);
             str_data[end] = '\0';
-            printf("%s\n", str_data);
-
+            // printf("%s\n", str_data);
+            
             // ===========================================================
             set_timespec(&times[1].tv_nsec, str_data, 1);
             set_timespec(&times[1].tv_sec, str_data, 0);
@@ -242,25 +260,40 @@ void restor_meta(char *path_file, struct database *db)
             set_timespec(&times[0].tv_sec, str_data, 0);
             set_timespec(&saved_dev, str_data, 2);
             set_timespec(&saved_inode, str_data, 3);
- 
+            
             if(lstat(str_data, &sb) == -1)
                 errExit("lstat");
-
+            
+            
             if (sb.st_ino == saved_inode && sb.st_dev == saved_dev) {
                 if (utimensat(AT_FDCWD, str_data, times, 0) == -1)
-                    errExit("utimensat");
+                errExit("utimensat");
             }
-
+            
+            if(lstat(str_data, &sb) == -1)
+                errExit("lstat");
+            
+            add_to_arr(str_data, &sb, db);
             // ===========================================================
             free(str_data);
-
+            
             offset += end + 1;
             if(lseek(fd, offset, SEEK_SET) == -1)
-                errExit("lseek");
+            errExit("lseek");
         }
     }
+    
+    if(flag_info)
+        print_info(db);  
 }
+void set_time(char *path_file, char *atime, char *mtime, struct database *db, int flag_info)
+{
+    struct stat sb;
+    struct timespec times[2];
+    int fd;
 
+    
+}
 
 int main(int argc, char *argv[])
 {
@@ -268,7 +301,9 @@ int main(int argc, char *argv[])
     struct stat sb;
     char path[MAX_PATH];
     char path_file[MAX_PATH];
-    int flag_restor = 0, flag_info = 0;
+    char atime[32] = {0};
+    char mtime[32] = {0};
+    int flag_save = 0, flag_restor = 0, flag_settime, flag_info = 1;
 
     database db = {};
     db.count = 0;
@@ -277,18 +312,13 @@ int main(int argc, char *argv[])
     if (db.files == NULL)
         errExit("malloc");
 
-    // "args": ["-q", "startup"],                                   // 1 аргумент / 2 аргумента
-    // "args": ["-tq", "startup", "atime", "mtime"],                // 4 аргумента / 5 аргументов. вместо времени можно написать "-"
-    // "args": ["-t", "-q", "startup", "atime", "mtime"],                // 4 аргумента / 5 аргументов. вместо времени можно написать "-"
-    // "args": ["-sq", "startup", "/tmp/rot"],                      // 3 аргумента / 4 аргумента
-    // "args": ["-rq", "startup", "/tmp/rot"],                      // 3 аргумента / 4 аргумента
     
     // ----------------------------------------------------------
     char args[32] = "";
     size_t j = 0;
     int first_non_option = 1;
 
-    for (; first_non_option < argc; ++first_non_option) {
+        for (; first_non_option < argc; ++first_non_option) {
 
         if (strcmp(argv[first_non_option], "--") == 0) {
             ++first_non_option;
@@ -297,41 +327,39 @@ int main(int argc, char *argv[])
 
         if (argv[first_non_option][0] != '-')
             break;
-
+        
         for (char *p = argv[first_non_option] + 1; *p; ++p)
             args[j++] = *p;
     }
 
     args[j] = '\0';
-
-    printf("flags = %s\n", args);
-    printf("first arg = %d\n", first_non_option);
     // ----------------------------------------------------------
 
-    char *tt = argv[1];
-    if(argc > 2) {
-        if(argv[1][0] == '-' && argv[1]) {
-            printf("y");
-        }
+
+    for (char *p = args; *p; ++p) {
+        if (*p == 'q')
+            flag_info = 0;
+        else if (*p == 't')
+            flag_settime = 1;
+        else if (*p == 's')
+            flag_save = 1;
+        else if (*p == 'r')
+            flag_restor = 1;
     }
 
-
-    if (argc < 2 || strcmp(argv[1], "--help") == 0) {
-        usageErr("%s file [-r] path [path]\n", argv[0]);
-    } else if (argc == 2) {
-        flag_info = 1;
-        strcpy(path, argv[1]);
-    } else if (argc == 3) {
-        strcpy(path, argv[1]);
-        strcpy(path_file, argv[2]);
-    } else if (argc == 4 && strstr(argv[1], "-r") != NULL) {
-        flag_restor = 1;
-        strcpy(path, argv[2]);
-        strcpy(path_file, argv[3]);
+    strcpy(path, argv[first_non_option]);
+    if (argc - first_non_option == 2) {
+        strcpy(path_file, argv[first_non_option + 1]);
+    } else if (argc - first_non_option == 3) {
+        strcpy(atime, argv[first_non_option + 1]);
+        strcpy(mtime, argv[first_non_option + 2]);
     }
 
-    if(flag_restor) {
-        restor_meta(path_file, &db);
+    if (flag_settime) {
+        set_time(path_file, atime, mtime, &db, flag_info);
+        return 0;
+    } else if(flag_restor) {
+        restor_meta(path_file, &db, flag_info);
         return 0;
     }
 
@@ -363,12 +391,7 @@ int main(int argc, char *argv[])
     
     free(full_path);
           
-    if(flag_info) {
-        print_info(&db);
-        return 0;
-    }
-
-    write_to_file(path_file, &db);
+    write_to_file(path_file, &db, flag_info);
 
     free_database(&db);
                 
