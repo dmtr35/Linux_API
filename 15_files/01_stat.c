68030f93bfb768fe2c9b618345983d278a269c49
dmtr35 <nuke-id@yandex.ru>
2026-07-20 19:19:59 +0300

    Auto-commit: 20-07-2026 19:19:59 Added:    15_files/03_chown.c, 15_files/04_chmod_access.c Modified: .vscode/tasks.json, 08_users_groups/getpwnam_getgrnam.c, 08_users_groups/getspnam_getspent.c, 08_users_groups/setpwent.c, 09_proccred/01_info.c, 15_files/01_stat.c, 15_files/exercises/access_euid.c, startup Deleted:  15_files/03_chmod.c, 15_files/04_access.c, @

--- 15_files/01_stat.c
+++ 15_files/01_stat.c
@@ -20,34 +20,46 @@ int fstat(int fd, struct stat *statbuf);
 void printf_stat(struct stat *statbuf)
 {
     printf(
-        "%-10s: %ld\n"
-        "%-10s: %ld\n"
-        "%-10s: %d\n"
-        "%-10s: %ld\n"
-        "%-10s: %d\n"
-        "%-10s: %d\n"
-        "%-10s: %ld\n"
-        "%-10s: %ld\n"
-        "%-10s: %ld\n"
-        "%-10s: %ld\n"
-        "%-10s: %ld\n"
-        "%-10s: %ld\n"
-        "%-10s: %ld\n",
-        "st_dev", statbuf->st_dev,          // Идентификатор устройства, на котором находится файл      st_dev    : 64769                             
-        "st_ino", statbuf->st_ino,          // Номер индексного дескриптора файла                       st_ino    : 5776159             
-        "st_mode", statbuf->st_mode,        // Тип файла и права доступа                                st_mode   : 33204 
-        "st_nlink", statbuf->st_nlink,      // Количество (жестких) ссылок на файл                      st_nlink  : 1             
-        "st_uid", statbuf->st_uid,          // Пользовательский ID владельца файла                      st_uid    : 1000             
-        "st_gid", statbuf->st_gid,          // Групповой ID владельца файла                             st_gid    : 1000     
-        "st_rdev", statbuf->st_rdev,        // Идентификаторы файлов устройств                          st_rdev   : 0         
-        "st_size", statbuf->st_size,        // Общий размер файла (в байтах)                            st_size   : 32     
-        "st_blksize", statbuf->st_blksize,  // Оптимальный размер блока для ввода-вывода (в байтах)     st_blksize: 4096                             
-        "st_blocks", statbuf->st_blocks,    // Количество отведенных блоков (по 512 байт)               st_blocks : 8                     
-        "st_atime", statbuf->st_atime,      // Время последнего доступа к файлу                         st_atime  : 1782223799         
-        "st_mtime", statbuf->st_mtime,      // Время последнего изменения файла                         st_mtime  : 1781455645         
-        "st_ctime", statbuf->st_ctime       // Время последнего изменения статуса                       st_ctime  : 1781455645             
+        "%-10s  %-10s: %ld\n"
+        "%-10s  %-10s: %ld\n"
+        "%-10s  %-10s: %o\n"
+        "%-10s  %-10s: %ld\n"
+        "%-10s  %-10s: %d\n"
+        "%-10s  %-10s: %d\n"
+        "%-10s  %-10s: %ld\n"
+        "%-10s  %-10s: %ld\n"
+        "%-10s  %-10s: %ld\n"
+        "%-10s  %-10s: %ld\n"
+        "%-10s  %-10s: %ld\n"
+        "%-10s  %-10s: %ld\n"
+        "%-10s  %-10s: %ld\n",
+        "dev_t", "st_dev", statbuf->st_dev,          // Идентификатор устройства, на котором находится файл      st_dev    : 64769                             
+        "ino_t", "st_ino", statbuf->st_ino,          // Номер индексного дескриптора файла                       st_ino    : 5776159             
+        "mode_t", "st_mode", statbuf->st_mode,        // Тип файла и права доступа                                st_mode   : 33204 
+        "nlink_t", "st_nlink", statbuf->st_nlink,      // Количество (жестких) ссылок на файл                      st_nlink  : 1             
+        "uid_t", "st_uid", statbuf->st_uid,          // Пользовательский ID владельца файла                      st_uid    : 1000             
+        "gid_t", "st_gid", statbuf->st_gid,          // Групповой ID владельца файла                             st_gid    : 1000     
+        "dev_t", "st_rdev", statbuf->st_rdev,        // Идентификаторы файлов устройств                          st_rdev   : 0         
+        "off_t", "st_size", statbuf->st_size,        // Общий размер файла (в байтах)                            st_size   : 32     
+        "blksize_t", "st_blksize", statbuf->st_blksize,  // Оптимальный размер блока для ввода-вывода (в байтах)     st_blksize: 4096                             
+        "blkcnt_t", "st_blocks", statbuf->st_blocks,    // Количество отведенных блоков (по 512 байт)               st_blocks : 8                     
+        "time_t", "st_atime", statbuf->st_atime,      // Время последнего доступа к файлу                         st_atime  : 1782223799         
+        "time_t", "st_mtime", statbuf->st_mtime,      // Время последнего изменения файла                         st_mtime  : 1781455645         
+        "time_t", "st_ctime", statbuf->st_ctime       // Время последнего изменения статуса                       st_ctime  : 1781455645             
     );
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*  Макросы для проверки типов файлов в поле st_mode структуры stat
     Константа           Проверочный макрос          Тип файла
--- 15_files/exercises/access_euid.c
+++ 15_files/exercises/access_euid.c
@@ -1,9 +1,11 @@
-#include <unistd.h>                         /* access */
+#define _GNU_SOURCE                         /* getresuid, getresgid */
+#include <unistd.h>                         /* access, getresuid, getresgid */
 #include <sys/stat.h>                       /* umask, chmod, fchmod, S_IRWXU, S_IRWXG, S_IRWXO */
 #include <sys/types.h>                      /* mode_t */
 #include <stdio.h>                          /* printf */
 #include <fcntl.h>                          /* open */
 #include <libgen.h>                         /* dirname, basename */
+#include <limits.h>                         /* NGROUPS_MAX */
 #include "../../lib/error_functions.h"
 #include "../../lib/file_perms.h"
 
@@ -19,10 +21,12 @@
 // int fchmod(int fd, mode_t mode);
 
 
+
+
 int check_access(char *pathname)
 {
     struct stat sb;
-    
+
     if (stat(pathname, &sb) == -1)
         errExit("stat");
 
@@ -41,6 +45,119 @@ int check_access(char *pathname)
     puts("================================================");
 }
 
+
+int check_gr(const struct stat *sb)
+{
+    int count_gr = 0;
+    gid_t grouplist[NGROUPS_MAX + 1];
+
+    count_gr = getgroups(NGROUPS_MAX, grouplist);
+    if (count_gr == -1)
+        return 0;
+
+    for (int i = 0; i < count_gr; ++i) {
+        if (sb->st_gid == grouplist[i]) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int check_perm(const char *pathname, struct stat *sb, int mode)
+{
+    int USR, GRP, OTH;
+    mode_t perm = sb->st_mode;
+    uid_t ruid, euid, suid;
+    gid_t rgid, egid, sgid;
+
+    uid_t uid_file = sb->st_uid & 0777;;
+    gid_t gid_file = sb->st_gid & 0777;;
+
+    if (mode == R_OK) {
+        USR = S_IRUSR;
+        GRP = S_IRGRP;
+        OTH = S_IROTH;
+    } else if (mode == W_OK) {
+        USR = S_IWUSR;
+        GRP = S_IWGRP;
+        OTH = S_IWOTH;
+    } else if (mode == X_OK) {
+        USR = S_IXUSR;
+        GRP = S_IXGRP;
+        OTH = S_IXOTH;
+    }
+
+    getresuid(&ruid, &euid, &suid);
+    getresgid(&rgid, &egid, &sgid);
+
+    if (uid_file == euid) {
+        if (perm & USR) {
+            printf("file %s читаемый\n", pathname);
+            return 0;
+        } else {
+            return -1;
+        }
+    } else if (gid_file == egid || check_gr(sb)) {
+        if (perm & GRP) {
+            printf("file %s читаемый\n", pathname);
+            return 0;
+        } else {
+            return -1;
+        }
+    } else {
+        if (perm & OTH) {
+            printf("file %s читаемый\n", pathname);
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+}
+реализовать:
+Настоящий access() имеет особое поведение для root.
+Если:
+uid == 0
+то:
+R_OK и W_OK обычно разрешены
+X_OK требует хотя бы одного execute-бита
+
+int my_access(const char *pathname, int mode)
+{
+    int res, flag_gr = 0, count_gr = 0;
+    struct stat sb;
+
+    res = stat(pathname, &sb);
+    if (res == -1)
+        return -1;
+
+    if (mode == F_OK) {
+        if (res == -1) {
+            printf("file %s НЕ существует\n", pathname);
+            return -1;
+        }
+    }
+
+    printf("file %s существует\n", pathname);
+    // printf("sb.st_uid: %d\n", sb.st_uid);
+    // printf("sb.st_gid: %d\n", sb.st_gid);
+    
+    // res = setregid(-1, 1000);
+    // res = setreuid(-1, 1000);
+
+    // getresuid(&ruid, &euid, &suid);
+    // getresgid(&rgid, &egid, &sgid);
+    // printf("ruid: %d, euid: %d, suid: %d\n", ruid, euid, suid);
+    // printf("rgid: %d, egid: %d, sgid: %d\n", rgid, egid, sgid);
+    
+    // printf("sb.st_uid: %d\n", sb.st_uid);
+    // printf("sb.st_gid: %d\n", sb.st_gid);
+
+    check_perm(pathname, &sb, R_OK);
+    check_perm(pathname, &sb, W_OK);
+    check_perm(pathname, &sb, X_OK);
+}
+
+
 int main(int argc, char *argv[])
 {
     int fd, res;
@@ -49,21 +166,26 @@ int main(int argc, char *argv[])
     
     char *pathname = argv[1];
 
-    if (access(pathname, F_OK) == 0)
-        if (unlink(pathname) == -1)
-            errMsg("unlink-%s", pathname);
+    // res = my_access(pathname, F_OK);
+    res = my_access(pathname, R_OK);
+    // res = my_access(pathname, W_OK);
+    // res = my_access(pathname, X_OK);
 
-    fd = open(pathname, O_RDWR | O_CREAT, 0000);
-    check_access(pathname);
+    // if (access(pathname, F_OK) == 0)
+    //     if (unlink(pathname) == -1)
+    //         errMsg("unlink-%s", pathname);
+
+    // fd = open(pathname, O_RDWR | O_CREAT, 0000);
+    // check_access(pathname);
     
-    mode = (sb.st_mode | S_IRGRP | S_IWGRP | S_IXGRP);
-    fchmod(fd, mode);
-    check_access(pathname);
-    mode = (sb.st_mode | S_IRUSR | S_IWUSR | S_IXUSR & ~S_IRGRP & ~S_IWGRP & ~S_IXGRP);
-    fchmod(fd, mode);
-    check_access(pathname);
+    // mode = (sb.st_mode | S_IRGRP | S_IWGRP | S_IXGRP);
+    // fchmod(fd, mode);
+    // check_access(pathname);
+    // mode = (sb.st_mode | S_IRUSR | S_IWUSR | S_IXUSR & ~S_IRGRP & ~S_IWGRP & ~S_IXGRP);
+    // fchmod(fd, mode);
+    // check_access(pathname);
 
 
-    close(fd);
+    // close(fd);
     return 0;
 }
